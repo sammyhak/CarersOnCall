81b2ba0b3e2dcc590713ae336392af8009b9ea48
samhak <samsonbaruwa6gmail.com>
2024-08-03 02:07:16 +0100

    service component 100% done

--- src/components/pagesComponent/services.jsx
+++ src/components/pagesComponent/services.jsx
@@ -1,12 +1,63 @@
+import { useEffect, useState } from "react";
 import prevButton from "../../assets/icons/backArrow.svg";
 import nextButton from "../../assets/icons/forwardArrow.svg";
 import service1 from "../../assets/service1.svg";
 import service2 from "../../assets/service2.svg";
 import service3 from "../../assets/service3.svg";
 import "../../styles/components/pagesComponents/services.scss";
+import HandleResize from "../../hooks/handleResize";
 
 const Services = () => {
-  const servicesList = [
+  const [itemsToDisplay, setitemsToDisplay] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [totalItemsToDisplay, settotalItemsToDisplay] = useState(0);
+  const [slideIn, setSlideIn] = useState(false);
+
+  const getScreenSize = HandleResize();
+
+  useEffect(() => {
+    setitemsToDisplay(3);
+    settotalItemsToDisplay(3);
+    if (getScreenSize <= 768) {
+      setitemsToDisplay(1);
+      settotalItemsToDisplay(1);
+    } else if (getScreenSize <= 992) {
+      setitemsToDisplay(2);
+      settotalItemsToDisplay(2);
+    }
+  }, [getScreenSize]);
+
+  const handlePrev = () => {
+    if (currentIndex >= itemsToDisplay) {
+      setCurrentIndex(currentIndex - itemsToDisplay);
+      settotalItemsToDisplay(totalItemsToDisplay - itemsToDisplay);
+      setSlideIn(false);
+
+      setTimeout(() => {
+        setSlideIn(true);
+      }, 300);
+    }
+  };
+
+  const handleNext = (e) => {
+    e.preventDefault();
+
+    if (totalItemsToDisplay < servicesList.length) {
+      setCurrentIndex(itemsToDisplay + currentIndex);
+      settotalItemsToDisplay(totalItemsToDisplay + itemsToDisplay);
+      setSlideIn(false);
+
+      setTimeout(() => {
+        setSlideIn(true);
+      }, 300);
+    }
+  };
+
+  useEffect(() => {
+    !slideIn && setSlideIn(true);
+  }, []);
+
+  const [servicesList] = useState([
     {
       img: service1,
       text: "Personal Care",
@@ -36,7 +87,33 @@ const Services = () => {
       img: service3,
       text: "Health Monitoring",
     },
-  ];
+    {
+      img: service1,
+      text: "Personal Care",
+    },
+
+    {
+      img: service2,
+      text: "Companionship",
+    },
+
+    {
+      img: service2,
+      text: "tESTO",
+    },
+
+    {
+      img: service2,
+      text: "SHIT",
+    },
+  ]);
+
+  const [slicedServicesList, setSlicedServiceList] = useState([]);
+
+  useEffect(() => {
+    setSlicedServiceList(servicesList.slice(currentIndex, totalItemsToDisplay));
+  }, [itemsToDisplay, totalItemsToDisplay, currentIndex]);
+
   return (
     <>
       <section className="services">
@@ -44,26 +121,41 @@ const Services = () => {
           <h2 className="services__title">Our Services</h2>
 
           <div className="controls d-flex">
-            <span className="controls__prev disabled">
+            <span
+              className={`controls__prev ${currentIndex === 0 && "disabled"}`}
+              onClick={handlePrev}
+            >
               <img src={prevButton} alt="prev" />
             </span>
 
-            <span className="controls__next">
+            <span
+              className={`controls__next ${
+                totalItemsToDisplay >= servicesList.length && "disabled"
+              }`}
+              onClick={handleNext}
+            >
               <img src={nextButton} alt="next" />
             </span>
           </div>
         </div>
+        <div
+          className={`services__content__container row ${slideIn && "slideIn"}`}
+        >
+          {slicedServicesList.length > 0 &&
+            slicedServicesList.map((service, index) => {
+              return (
+                <div
+                  key={index}
+                  className="services__content col-lg-4 col-md-6 col-sm-12"
+                >
+                  <div className="services__content_img">
+                    <img src={service.img} alt="service image" />
+                  </div>
 
-        <div className="services__content__container row">
-          {servicesList.map((service, index) => {
-            return (
-              <div className="services__content col-lg-4 col-md-6 col-sm-12">
-                <img src={service.img} alt="service image" />
-
-                <h6>{service.text}</h6>
-              </div>
-            );
-          })}
+                  <h6>{service.text}</h6>
+                </div>
+              );
+            })}
         </div>
       </section>
     </>
